a2ce6446647a3818f7aa0c15d2d981fe7ee2f8f6
yongj.zhuang <yongj.zhuang@outlook.com>
2024-07-05 16:41:26 +0800

    tool scripts

---
+++ tools/centos_cn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+download_qkserve="https://mirror.ghproxy.com/https://github.com/CurtisNewbie/qkserve/releases/download/v0.0.1/qkserve_linux_v0.0.1"
+file="$1"
+if [ ! -f "/tmp/qkserve" ]; then
+    yum install -y curl procps \
+        && curl "$download_qkserve" -L -o /tmp/qkserve \
+        && chmod +x /tmp/qkserve
+fi
+./tmp/qkserve -file "$file" -port 443

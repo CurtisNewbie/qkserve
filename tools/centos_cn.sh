4cc3abf696a605d8baba55c4aea7c0434d03c45a
yongj.zhuang <yongj.zhuang@outlook.com>
2024-07-08 09:54:48 +0800

    Modified:  tools/centos.sh Modified:  tools/centos_cn.sh

--- tools/centos_cn.sh
+++ tools/centos_cn.sh
@@ -2,9 +2,15 @@
 
 download_qkserve="https://mirror.ghproxy.com/https://github.com/CurtisNewbie/qkserve/releases/download/v0.0.1/qkserve_linux_v0.0.1"
 file="$1"
-if [ ! -f "/tmp/qkserve" ]; then
-    yum install -y curl procps \
-        && curl "$download_qkserve" -L -o /tmp/qkserve \
-        && chmod +x /tmp/qkserve
+qkserve="/qkserve"
+if [ ! -f "$qkserve" ]; then
+    qkserve="/tmp/qkserve"
 fi
-/tmp/qkserve -file "$file" -port 443
+
+if [ ! -f "$qkserve" ]; then
+    yum install -y curl procps \
+        && curl "$download_qkserve" -L -o "$qkserve" \
+        && chmod +x "$qkserve"
+fi
+chmod +x $qkserve
+$qkserve -file "$file" -port 443
